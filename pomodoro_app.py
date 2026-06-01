ac3fb83cac30f001e25a915966f220253effa76d
sduong07 <simon.duong@uoit.net>
2026-06-01 00:21:54 -0400

    Add files via upload

--- pomodoro_app.py
+++ pomodoro_app.py
@@ -44,18 +44,6 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 st.markdown(top_bar_css, unsafe_allow_html=True)
 
 
-st.markdown("""
-<style>
-[data-testid="stExpander"] details {
-    background-color: #f5f5f5;
-}
-
-[data-testid="stExpander"] details[open] {
-    background-color: #f5f5f5;
-}
-</style>
-""", unsafe_allow_html=True)
-
 #Injects custom css styling(Font to Pooppins, reduces top padding, defining styles for .main-header and.section-heading)
 st.markdown("""
 <style>
